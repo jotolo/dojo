06f10cb35774ee7b1b7ded89ecce87c851d7fda7
Jorge T <jotolo7@gmail.com>
2017-08-16 04:48:07 -0400

    Scaffold Project

---
+++ app/models/stack_exchange.rb
@@ -0,0 +1,32 @@
+class StackExchange
+  include HTTParty
+  base_uri 'overwatch-api.net'
+
+  def initialize(service, page)
+    @options = { query: { site: service, page: page } }
+  end
+
+  def heroes
+    self.class.get('/api/v1/hero', @options)
+  end
+
+  def hero(hero_id)
+    if hero_id.nil?
+      nil
+    else
+      self.class.get("/api/v1/hero/#{hero_id}", @options)
+    end
+  end
+
+  def abilities
+    self.class.get('/api/v1/hero', @options)
+  end
+
+  def ability(ability_id)
+    if ability_id.nil?
+      nil
+    else
+      self.class.get("/api/v1/ability#{ability_id}", @options)
+    end
+  end
+end
